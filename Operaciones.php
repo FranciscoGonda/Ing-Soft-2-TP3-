a7311ab2f02659cf8b2c48e2275fe295ca1a4f53
LihuesDash <lihuen935@hotmail.com>
2021-09-10 14:45:07 -0300

    Subiendo los primeros archivos

---
+++ Operaciones.php
@@ -0,0 +1,19 @@
+<?php 
+$n1 = $_GET['n1'];
+$n2 = $_GET['n2'];
+
+$multiplicacion = $n1 * $n2
+?>
+<!DOCTYPE html> 
+<html> 
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title></title>
+	
+	</head>
+	<body>
+		<h1>Resultado</h1>
+		<?php echo "El resultado es $n1 * $n2 = $multiplicacion </p>"?> 
+	</body>
+</html>
